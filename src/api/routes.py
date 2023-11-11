6cfec1bee6e11efc6c8a8ca9150fa8cba3a34d90
JohnD42 <139003527+JohnD42@users.noreply.github.com>
2023-11-11 03:27:48 +0000

    adjusted to have dog walks, pet check-ins, and meet and greets combined in API endpoint since all can conflict with each other

--- src/api/routes.py
+++ src/api/routes.py
@@ -33,9 +33,9 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
-@api.route('/schedule-walk', methods=['POST'])
+@api.route('/schedule-walk-or-check-in-or-meet-and-greet', methods=['POST'])
 @jwt_required()
-def handle_schedule_walk_or_check_in():
+def handle_schedule_walk_or_check_in_or_meet_and_greet():
     current_user_id = get_jwt_identity()
     user = User.query.get(current_user_id)
     user_address = user["address"]
@@ -82,9 +82,9 @@ def handle_schedule_walk_or_check_in():
         print(f"An error occurred: {error}")
 
 
-    walk_or_check_in = req["type"]
+    wlk_or_check_or_mt_and_grt = req["type"]
 
-    msg = walk_or_check_in + "scheduled successfully."
+    msg = wlk_or_check_or_mt_and_grt + "scheduled successfully."
 
     url = "https://calendar.google.com/calendar/ical/f73ae5b685428f5ee9f2e95b1b39fe17de1f5851e48ab7ddd2dd0ad3765c0f5d%40group.calendar.google.com/public/basic.ics"
 
@@ -112,67 +112,6 @@ def handle_schedule_pet_sitting():
     SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 
     calendar_id = "564074f66734a91ee109c5d45a58ad814986316b76f2059642ac08bb37b7acb5@group.calendar.google.com"
-
-    try:
-        service = build("calendar", "v3", credentials=creds)
-
-        # Call the Calendar API
-        now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
-        print("Getting the upcoming 10 events")
-        events_result = (
-            service.events()
-            .list(
-                calendarId=calendar_id,
-                timeMin=now,
-                maxResults=10,
-                singleEvents=True,
-                orderBy="startTime",
-            )
-            .execute()
-        )
-        events = events_result.get("items", [])
-
-        if not events:
-            print("No upcoming events found.")
-            pass
-
-        # Prints the start and name of the next 10 events
-        for event in events:
-            start = event["start"].get("dateTime", event["start"].get("date"))
-            print(start, event["summary"])
-
-    except HttpError as error:
-        print(f"An error occurred: {error}")
-
-
-
-    url = "https://calendar.google.com/calendar/ical/564074f66734a91ee109c5d45a58ad814986316b76f2059642ac08bb37b7acb5%40group.calendar.google.com/public/basic.ics"
-
-
-    response_body = {
-        "msg": "Pet sitting scheduled successfully.",
-        "status": "ok"
-    }
-
-    return jsonify(response_body), 200
-
-@api.route('/schedule-meet-and-greet', methods=['POST'])
-@jwt_required()
-def handle_schedule_pet_sitting():
-
-    current_user_id = get_jwt_identity()
-    user = User.query.get(current_user_id)
-    user_address = user["address"]
-
-    req = request.get_json()
-
-    SCOPES = ['https://www.googleapis.com/auth/calendar']
-    SERVICE_ACCOUNT_FILE = 'credentials.json'
-
-    creds = service_account.Credentials.from_service_account_file(
-    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-
-    calendar_id = "3bf9db1f1eec427536bcea09c6095d5cadf05f182daee1b7dba63b47aed66344@group.calendar.google.com"
 
     try:
         service = build("calendar", "v3", credentials=creds)
