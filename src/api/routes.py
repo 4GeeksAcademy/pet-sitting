7e74ba6a16ee7942de314ad0c1216a811a10a1c5
Whitney Heacock <134784015+Swtpea1516@users.noreply.github.com>
2023-11-12 01:25:28 +0000

    stuff

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,22 +1,74 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
-from flask import Flask, request, jsonify, url_for, Blueprint
+from flask import Flask, request, jsonify, Blueprint
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from werkzeug.security import generate_password_hash, check_password_hash
 from api.models import db, User
-from api.utils import generate_sitemap, APIException
-from flask_cors import CORS
+from api.utils import APIException
 
+
+# Create the Blueprint
 api = Blueprint('api', __name__)
 
-# Allow CORS requests to this API
-CORS(api)
+@api.route('/protected', methods=['GET'])
+@jwt_required()
+def protected():
+    try:
+        current_user = get_jwt_identity()
+        return jsonify(message=f'Hello, {current_user}!')
+    except Exception as e:
+        return jsonify(message="Missing Authorization Header or Invalid Token"), 401
 
+@api.route('/signup', methods=['POST'])
+def signup():
+    body = request.get_json()
+    if "email" not in body or "password" not in body:
+        raise APIException("Please provide both email and password", status_code=400)
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+    email = body['email']
+    password = body['password']
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+    existing_user = User.query.filter_by(email=email).first()
+    if existing_user:
+        return jsonify(message="User already exists"), 400
 
-    return jsonify(response_body), 200
+    hashed_password = generate_password_hash(password)
+    new_user = User(email=email, password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(message="Successfully created user"), 200
+
+@api.route('/login', methods=['POST'])
+def login():
+    body = request.get_json()
+    if "email" not in body or "password" not in body:
+        raise APIException("Please provide both email and password", status_code=400)
+
+    email = body['email']
+    password = body['password']
+
+    user = User.query.filter_by(email=email).first()
+
+    if user and check_password_hash(user.password, password):
+        access_token = create_access_token(identity=email)
+        return jsonify(access_token=access_token), 200
+    else:
+        return jsonify(message="Login failed. Please check your credentials."), 401
+
+@api.route('/user', methods=['GET'])
+@jwt_required()
+def get_user():
+    current_user = get_jwt_identity()
+    user = User.query.filter_by(email=current_user).first()
+
+    if user:
+        return jsonify(user.serialize()), 200
+    else:
+        return jsonify(message="User not found"), 404
+
+@api.route('/logout', methods=['DELETE'])
+@jwt_required()
+def logout():
+    return jsonify(message="Logged out successfully"), 200
+
+if __name__ == "__main__":
+    api.run()
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    window.location.href = "/home";
+  };
+
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="dropdown">
+        <button
+          className="btn btn-secondary dropdown-toggle"
+          type="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        > Login</button>
+        <ul className="dropdown-menu">
+          <li>
+            <a className="dropdown-item" href="#" onClick={handleLogout}>
+              Logout
+            </a>
+          </li>
+          <li>
+            <Link to="/login">
+              <a className="dropdown-item" href="#">
+                Login
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link to="/signup">
+              <a className="dropdown-item" href="#">
+                Signup
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
+
+
+
+
+
+
+
+
+
+
+// import { Link } from "react-router-dom";
+
+// export const Navbar = () => {
+// 	return (
+// 		<nav className="navbar navbar-light bg-light">
+// 		<div class="dropdown">
+//   <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+//     Login
+//   </button>
+//   <ul class="dropdown-menu">
+//     <li><Link to="/login"><a class="dropdown-item" href="#">Login</a></Link></li>
+//     <li><Link to="/signup"><a class="dropdown-item" href="#">Signup</a></Link></li>
+//     <li><a class="dropdown-item" href="#">Logout</a></li>
+//   </ul>
+// </div>
+// 		</nav>
+// 	);
+// };
