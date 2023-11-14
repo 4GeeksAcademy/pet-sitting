c49fce542222a0fa5af3aa1dd4ed8b937e6199c0
John Durtka <139003527+JohnD42@users.noreply.github.com>
2023-11-14 09:07:56 +0000

    started working, example html in place for sidebar on schedule

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -6,6 +6,7 @@ import { BackendURL } from "./component/backendURL";
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
+import { Schedule } from './pages/schedule.js'
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -27,6 +28,7 @@ const Layout = () => {
                     <Routes>
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
+                        <Route element={<Schedule />} path="/schedule-dog-walk" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
