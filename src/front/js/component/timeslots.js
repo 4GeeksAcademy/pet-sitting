5756921bef83b4f8495b8d090a1a614d0007c780
John Durtka <139003527+JohnD42@users.noreply.github.com>
2023-11-16 00:49:34 +0000

    added top margin to calendar

--- src/front/js/component/timeslots.js
+++ src/front/js/component/timeslots.js
@@ -4,59 +4,59 @@ import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 export const Timeslots = () => {
-	const { store, actions } = useContext(Context);
+	// const { store, actions } = useContext(Context);
 
-	const [namesOfCurrentDaysOfWeek, setNamesOfCurrentDaysOfWeek] = useState([])
+	// const [namesOfCurrentDaysOfWeek, setNamesOfCurrentDaysOfWeek] = useState([])
 
-	const startDayData = store.timeSlotsStartingDay
-	const namesOfDays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+	// const startDayData = store.timeSlotsStartingDay
+	// const namesOfDays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
 
-	const isLeapYear = (year) => {
-        return (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) || (year % 100 === 0 && year % 400 ===0)
-    }
+	// const isLeapYear = (year) => {
+    //     return (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) || (year % 100 === 0 && year % 400 ===0)
+    // }
 
-    const getFebDays = (year) => {
-        return isLeapYear(year) ? 29 : 28
-    }
-    const numDaysOfMonth = [31, getFebDays(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    // const getFebDays = (year) => {
+    //     return isLeapYear(year) ? 29 : 28
+    // }
+    // const numDaysOfMonth = [31, getFebDays(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-	const weekDatesRange = [...Array(7).keys()].map(i => i + startDayData.date)
+	// const weekDatesRange = [...Array(7).keys()].map(i => i + startDayData.date)
 
-	namesOfCurrentDaysOfWeek = []
+	// namesOfCurrentDaysOfWeek = []
 
-	const fixDatesAndSetDayNames = () => {
-		const pushDay = (day) => {
-			if(namesOfDaysOfWeek.length < 7) {
-				setNamesOfCurrentDaysOfWeek([...namesOfCurrentDaysOfWeek,day])
-			} else {
-				setNamesOfCurrentDaysOfWeek([])
-				setNamesOfCurrentDaysOfWeek([...namesOfCurrentDaysOfWeek,day])
-			}
-		}
+	// const fixDatesAndSetDayNames = () => {
+	// 	const pushDay = (day) => {
+	// 		if(namesOfDaysOfWeek.length < 7) {
+	// 			setNamesOfCurrentDaysOfWeek([...namesOfCurrentDaysOfWeek,day])
+	// 		} else {
+	// 			setNamesOfCurrentDaysOfWeek([])
+	// 			setNamesOfCurrentDaysOfWeek([...namesOfCurrentDaysOfWeek,day])
+	// 		}
+	// 	}
 
-		for(date of weekDatesRange) {
-			if(date > numDaysOfMonth[startDayData.month]) {
-				date = date - numDaysOfMonth[startDayData.month]
-				const dayInd = new Date(startDayData.year, startDayData.month + 1, date).getDay()
-				const dayOfWeek = namesOfDays[dayInd]
-				pushDay(dayOfWeek)
-			} else {
-				const dayInd = new Date(startDayData.year, startDayData.month + 1, date).getDay()
-				const dayOfWeek = namesOfDays[dayInd]
-				pushDay(dayOfWeek)
-			}
-		}
+	// 	for(date of weekDatesRange) {
+	// 		if(date > numDaysOfMonth[startDayData.month]) {
+	// 			date = date - numDaysOfMonth[startDayData.month]
+	// 			const dayInd = new Date(startDayData.year, startDayData.month + 1, date).getDay()
+	// 			const dayOfWeek = namesOfDays[dayInd]
+	// 			pushDay(dayOfWeek)
+	// 		} else {
+	// 			const dayInd = new Date(startDayData.year, startDayData.month + 1, date).getDay()
+	// 			const dayOfWeek = namesOfDays[dayInd]
+	// 			pushDay(dayOfWeek)
+	// 		}
+	// 	}
 
-		
-	}
-
-	useEffect(() => {
-		fixDatesAndSetDayNames()
-	},[])
-
-	// const createDaysOfWeekDivs = () => {
 		
 	// }
+
+	// useEffect(() => {
+	// 	fixDatesAndSetDayNames()
+	// },[])
+
+	// // const createDaysOfWeekDivs = () => {
+		
+	// // }
 	
 
 	return (
