88a89bd637e351b44df94d80948a72d52fef6c70
IvanStacy <139801928+IvanStacy@users.noreply.github.com>
2023-11-20 19:14:06 +0000

    showing the pdf file on the page

--- src/front/js/pages/insurance.js
+++ src/front/js/pages/insurance.js
@@ -6,10 +6,11 @@ export const Insurance = () => {
     return (
 
       <div className="container">
+         <iframe src={InsurancePDF}  style={{width: "100%", height: "900px"}} className="mb-3"/>
         <div className="card text-center mb-3 w: 25rem">
           <div className="card-body">
             <h5 className="card-title">Pet insurance</h5>
-            <a href={InsurancePDF} download="Insurance.pdf" className="btn btn-primary">Go somewhere</a>
+            <a href={InsurancePDF} download="Insurance.pdf" className="btn btn-success">Download file</a>
           </div>
         </div>
         </div>
