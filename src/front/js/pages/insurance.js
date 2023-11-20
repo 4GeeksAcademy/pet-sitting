92b3a00db26d51ad16d2f5820b8e639cf80cd6d6
IvanStacy <139801928+IvanStacy@users.noreply.github.com>
2023-11-20 18:49:14 +0000

    done with downloading pdf file

--- src/front/js/pages/insurance.js
+++ src/front/js/pages/insurance.js
@@ -1,18 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
+import InsurancePDF from "../../../../public/insurance.pdf";
+
 
 export const Insurance = () => {
-
-
     return (
+
       <div className="container">
         <div className="card text-center mb-3 w: 25rem">
           <div className="card-body">
             <h5 className="card-title">Pet insurance</h5>
-            <a href="../../img/insurance.pdf" download="Insurance.pdf" className="btn btn-primary">Go somewhere</a>
+            <a href={InsurancePDF} download="Insurance.pdf" className="btn btn-primary">Go somewhere</a>
           </div>
         </div>
         </div>
     )
 
 }
-
