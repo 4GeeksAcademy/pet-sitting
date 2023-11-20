075f6e99bdd9fb0a391de15a24c7da3821932337
IvanStacy <139801928+IvanStacy@users.noreply.github.com>
2023-11-20 23:18:52 +0000

    delete the download button

--- src/front/js/pages/insurance.js
+++ src/front/js/pages/insurance.js
@@ -7,13 +7,8 @@ export const Insurance = () => {
 
       <div className="container">
          <iframe src={InsurancePDF}  style={{width: "100%", height: "900px"}} className="mb-3"/>
-        <div className="card text-center mb-3 w: 25rem">
-          <div className="card-body">
-            <h5 className="card-title">Pet insurance</h5>
-            <a href={InsurancePDF} download="Insurance.pdf" className="btn btn-success">Download file</a>
-          </div>
-        </div>
-        </div>
+
+      </div>
     )
 
 }
