2c4a07d16ac5615eb11a2e9fb0175e86eaf7b6bf
John Durtka <139003527+JohnD42@users.noreply.github.com>
2023-11-24 22:27:28 +0000

    successfully merged with working login and sign up system

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -38,7 +38,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 						body: JSON.stringify({ email, password }),
 					};
 
-					const response = await fetch("https://glowing-spork-pjrjwg7vvwjw2rvwj-3001.app.github.dev/api/login", options);
+					const response = await fetch(getStore().backendURL + "/api/login", options);
 
 					if (response.status === 200) {
 						const data = await response.json();
@@ -67,7 +67,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 			getMessage: async () => {
 				try {
 					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
+					const resp = await fetch(getStore().backendURL + "/api/hello")
 					const data = await resp.json()
 					setStore({ message: data.message })
 					// don't forget to return something, that is how the async resolves
@@ -78,7 +78,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 			},
 			signup: async (formData) => {
 				try {
-					let response = await fetch("https://glowing-spork-pjrjwg7vvwjw2rvwj-3001.app.github.dev/api/signup", {
+					let response = await fetch(getStore().backendURL + "/api/signup", {
 						method: "POST",
 						headers: { "Content-Type": "application/json" },
 						body: JSON.stringify({ "email": formData.email, "password": formData.password, "first_name": formData.first_name, "last_name": formData.last_name })
