75b5dbeca22c68434829ba8c932694e30e2ec1f9
JohnD42 <139003527+JohnD42@users.noreply.github.com>
2023-11-25 22:46:47 +0000

    attempted to implement google calendar event scheduling on backend routes

--- src/api/routes.py
+++ src/api/routes.py
@@ -150,21 +150,18 @@ def handle_get_dog_walk_sched():
                 .execute()
             )
         events = events_result.get("items", [])
-        events = [{'start': event['start'],'end': event['end'],'summary': event['summary'], 'owned': True if user_email in event['summary'] else False} for event in events]
+        events = [{'start': event['start'],'end': event['end'],'summary': event['summary'].split(' ')[0:(len(event['summary'].split()) - 2)].join(' '), 'owned': True if user_email in event['summary'] else False} for event in events]
         return jsonify({'events': events, 'status': 'ok'}), 200
     except:
         return jsonify({'msg': 'Could not access the calendar'}), 404
-        
-
-    
-    
 
 @api.route('/get-meeting', methods=['POST'])
-#@jwt_required()
+@jwt_required()
 def handle_meeting_sched():
+    user_email = get_jwt_identity()
     req = request.get_json()
-    minTime = req.minTime
-    maxTime = req.maxTime
+    minTime = req['minTime']
+    maxTime = req['maxTime']
     try: 
         SCOPES = ['https://www.googleapis.com/auth/calendar']
         SERVICE_ACCOUNT_FILE = 'credentials.json'
@@ -187,16 +184,18 @@ def handle_meeting_sched():
                 .execute()
             )
         events = events_result.get("items", [])
+        events = [{'start': event['start'],'end': event['end'],'summary': event['summary'].split(' ')[0:(len(event['summary'].split()) - 2)].join(' '), 'owned': True if user_email in event['summary'] else False} for event in events]
         return jsonify({'events': events, 'status': 'ok'}), 200
     except:
         return jsonify({'msg': 'Could not access the calendar'}), 404
 
 @api.route('/get-pet-check-in', methods=['POST'])
-#@jwt_required()
+@jwt_required()
 def handle_get_pet_check_in_sched():
+    user_email = get_jwt_identity()
     req = request.get_json()
-    minTime = req.minTime
-    maxTime = req.maxTime
+    minTime = req['minTime']
+    maxTime = req['maxTime']
     try: 
         SCOPES = ['https://www.googleapis.com/auth/calendar']
         SERVICE_ACCOUNT_FILE = 'credentials.json'
@@ -219,25 +218,27 @@ def handle_get_pet_check_in_sched():
                 .execute()
             )
         events = events_result.get("items", [])
+        events = [{'start': event['start'],'end': event['end'],'summary': event['summary'].split(' ')[0:(len(event['summary'].split()) - 2)].join(' '), 'owned': True if user_email in event['summary'] else False} for event in events]
         return jsonify({'events': events, 'status': 'ok'}), 200
     except:
         return jsonify({'msg': 'Could not access the calendar'}), 404
 
 @api.route('/get-pet-sitting', methods=['POST'])
-#@jwt_required()
+@jwt_required()
 def handle_get_pet_sitting_sched():
+    user_email = get_jwt_identity()
     req = request.get_json()
-    minTime = req.minTime
-    maxTime = req.maxTime
+    minTime = req['minTime']
+    maxTime = req['maxTime']
     try: 
         SCOPES = ['https://www.googleapis.com/auth/calendar']
         SERVICE_ACCOUNT_FILE = 'credentials.json'
 
         creds = service_account.Credentials.from_service_account_file(
         SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-        service = build("calendar", "v3", credentials=creds)
         calendar_id = "564074f66734a91ee109c5d45a58ad814986316b76f2059642ac08bb37b7acb5@group.calendar.google.com"
-        
+
+        service = build("calendar", "v3", credentials=creds)
         events_result = (
                 service.events()
                 .list(
@@ -251,16 +252,17 @@ def handle_get_pet_sitting_sched():
                 .execute()
             )
         events = events_result.get("items", [])
+        events = [{'start': event['start'],'end': event['end'],'summary': event['summary'].split(' ')[0:(len(event['summary'].split()) - 2)].join(' '), 'owned': True if user_email in event['summary'] else False} for event in events]
         return jsonify({'events': events, 'status': 'ok'}), 200
     except:
         return jsonify({'msg': 'Could not access the calendar'}), 404
 
 @api.route('/schedule-walk-or-check-in-or-meet-and-greet', methods=['POST'])
-# @jwt_required()
+@jwt_required()
 def handle_schedule_walk_or_check_in_or_meet_and_greet():
-    # current_user_id = get_jwt_identity()
-    # user = User.query.get(current_user_id)
-    # user_address = user["address"]
+    email = get_jwt_identity()
+    user = User.query.filter_by(email=email).first()
+    user_address = user["address"]
 
     req = request.get_json()
 
@@ -272,58 +274,47 @@ def handle_schedule_walk_or_check_in_or_meet_and_greet():
 
     calendar_id = "f73ae5b685428f5ee9f2e95b1b39fe17de1f5851e48ab7ddd2dd0ad3765c0f5d@group.calendar.google.com"
 
+
     try:
         service = build("calendar", "v3", credentials=creds)
+        type_of_booking = req["type"]
+        details = req["details"]
+        pets = req["pets"]
+        start_time = req["startTime"]
+        end_time = req["endTime"]
+        recurring = req["recurring"]
+        recurring_count = req["recurring-count"]
 
-        # Call the Calendar API
-        now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
-        print("Getting the upcoming 10 events")
-        events_result = (
-            service.events()
-            .list(
-                calendarId=calendar_id,
-                timeMin=now,
-                maxResults=10,
-                singleEvents=True,
-                orderBy="startTime",
-            )
-            .execute()
-        )
-        events = events_result.get("items", [])
+        event = {
+                'summary': type_of_booking + 'with' + pets + 'for' + email,
+                'location': user_address,
+                'description': details,
+                'start': {
+                    'dateTime': start_time,
+                    'timeZone': 'America/Denver',
+                },
+                'end': {
+                    'dateTime': end_time,
+                    'timeZone': 'America/Denver',
+                },
+                'recurrence': [
+                    'RRULE:FREQ=' + recurring + ';COUNT=' + recurring_count
+                ],
+            }
 
-        if not events:
-            print("No upcoming events found.")
-            pass
-
-        # Prints the start and name of the next 10 events
-        for event in events:
-            start = event["start"].get("dateTime", event["start"].get("date"))
-            print(start, event["summary"])
-
-    except HttpError as error:
-        print(f"An error occurred: {error}")
-
-
-    wlk_or_check_or_mt_and_grt = req["type"]
-
-    msg = wlk_or_check_or_mt_and_grt + "scheduled successfully."
-
-    url = "https://calendar.google.com/calendar/ical/f73ae5b685428f5ee9f2e95b1b39fe17de1f5851e48ab7ddd2dd0ad3765c0f5d%40group.calendar.google.com/public/basic.ics"
-
-    response_body = {
-        "msg": msg,
-        "status": "ok"
-    }
+        event = service.events().insert(calendarId=calendar_id, body=event).execute()
+        return(jsonify({"msg": "Booking created successfully.", "status": "ok"})), 200
     
-    return jsonify(response_body), 200
+    except HttpError as error:
+        return(jsonify({"msg": "An error occurred: " + {error}})), 404
+
 
 @api.route('/schedule-pet-sitting', methods=['POST'])
 # @jwt_required()
 def handle_schedule_pet_sitting():
-
-    # current_user_id = get_jwt_identity()
-    # user = User.query.get(current_user_id)
-    # user_address = user["address"]
+    email = get_jwt_identity()
+    user = User.query.filter_by(email=email).first()
+    user_address = user["address"]
 
     req = request.get_json()
 
@@ -333,90 +324,38 @@ def handle_schedule_pet_sitting():
     creds = service_account.Credentials.from_service_account_file(
     SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 
+
     calendar_id = "564074f66734a91ee109c5d45a58ad814986316b76f2059642ac08bb37b7acb5@group.calendar.google.com"
 
     try:
         service = build("calendar", "v3", credentials=creds)
+        type_of_booking = req["type"]
+        details = req["details"]
+        pets = req["pets"]
+        start_time = req["startTime"]
+        end_time = req["endTime"]
+        recurring = req["recurring"]
+        recurring_count = req["recurring-count"]
 
-        # Call the Calendar API
-        now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
-        print("Getting the upcoming 10 events")
-        events_result = (
-            service.events()
-            .list(
-                calendarId=calendar_id,
-                timeMin=now,
-                maxResults=10,
-                singleEvents=True,
-                orderBy="startTime",
-            )
-            .execute()
-        )
-        events = events_result.get("items", [])
+        event = {
+                'summary': type_of_booking + 'with' + pets + 'for' + email,
+                'location': user_address,
+                'description': details,
+                'start': {
+                    'dateTime': start_time,
+                    'timeZone': 'America/Denver',
+                },
+                'end': {
+                    'dateTime': end_time,
+                    'timeZone': 'America/Denver',
+                },
+                'recurrence': [
+                    'RRULE:FREQ=' + recurring + ';COUNT=' + recurring_count
+                ],
+            }
 
-        if not events:
-            print("No upcoming events found.")
-            pass
-
-        # Prints the start and name of the next 10 events
-        for event in events:
-            start = event["start"].get("dateTime", event["start"].get("date"))
-            print(start, event["summary"])
-
+        event = service.events().insert(calendarId=calendar_id, body=event).execute()
+        return(jsonify({"msg": "Booking created successfully.", "status": "ok"})), 200
+    
     except HttpError as error:
-        print(f"An error occurred: {error}")
-
-
-
-    url = "https://calendar.google.com/calendar/ical/564074f66734a91ee109c5d45a58ad814986316b76f2059642ac08bb37b7acb5%40group.calendar.google.com/public/basic.ics"
-
-
-    response_body = {
-        "msg": "Pet sitting scheduled successfully.",
-        "status": "ok"
-    }
-
-    return jsonify(response_body), 200
-
-SCOPES = ['https://www.googleapis.com/auth/calendar']
-SERVICE_ACCOUNT_FILE = 'credentials.json'
-
-creds = service_account.Credentials.from_service_account_file(
-SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-
-calendar_id = "564074f66734a91ee109c5d45a58ad814986316b76f2059642ac08bb37b7acb5@group.calendar.google.com"
-
-try:
-    service = build("calendar", "v3", credentials=creds)
-
-    # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
-    print("Getting the upcoming 10 events")
-    events_result = (
-        service.events()
-        .list(
-            calendarId=calendar_id,
-            timeMin=now,
-            maxResults=10,
-            singleEvents=True,
-            orderBy="startTime",
-        )
-        .execute()
-    )
-    events = events_result.get("items", [])
-
-    if not events:
-        print("No upcoming events found.")
-        pass
-
-    # Prints the start and name of the next 10 events
-    for event in events:
-        start = event["start"].get("dateTime", event["start"].get("date"))
-        print(start, event["summary"])
-
-except HttpError as error:
-    print(f"An error occurred: {error}")
-
-
-
-url = "https://calendar.google.com/calendar/ical/564074f66734a91ee109c5d45a58ad814986316b76f2059642ac08bb37b7acb5%40group.calendar.google.com/public/basic.ics"
+        return(jsonify({"msg": "An error occurred: " + {error}})), 404
