8bb399184bc90c85302afe9c19c0a7fffe59129e
John Durtka <139003527+JohnD42@users.noreply.github.com>
2023-12-08 22:16:13 +0000

    saving progress before pulling from development

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,53 +1,102 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			backendURL: process.env.BACKEND_URL,
+            token: null,
+            timeSlotsStartingDay: {
+                "date": (new Date).getDate(),
+                "month": (new Date).getMonth(),
+                "year": (new Date).getFullYear()
+            },
+            activeScheduleTab: "nav-timeslots",
 			token: null,
-			timeSlotsStartingDay: {
-				"date": (new Date).getDate(),
-				"month": (new Date).getMonth(),
-				"year": (new Date).getFullYear()
-			},
-			activeScheduleTab: "nav-timeslots",
 			payPalToken: null,
 			paymentSuccessful: false,
-			activeScheduleTab: "nav-timeslots",
-			typeOfSchedule: 'dog-walk'
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
+            changeActiveScheduleTab: (payload) => {
+                setStore({ activeScheduleTab: payload })
+            },
 			login: async (email, password) => {
-				const store = getStore();
-				try {
-					let options = {
-						method: "POST",
-						headers: {
-							'Content-Type': 'application/json',
-						},
-						body: JSON.stringify({ email, password }),
-					};
-					const response = await fetch(process.env.BACKEND_URL + "api/login", options);
-					console.log('Login response:', response);
-					if (response.status === 200) {
-						const data = await response.json();
-						console.log("access token", data.access_token);
-						sessionStorage.setItem("token", data.access_token);
-						sessionStorage.setItem("email", email);
-						setStore({
-							token: data.access_token,
-							email: email,
-						});
-						return true;
-					} else {
-						console.error("Login failed. Please check your credentials.");
-						return false;
-					}
-				} catch (error) {
-					console.error("Login error:", error);
-					alert("An error occurred during login.");
-					return false;
-				}
-			},
+                const store = getStore();
+                try {
+                    let options = {
+                        method: "POST",
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({ email, password }),
+                    };
+                    const response = await fetch(process.env.BACKEND_URL + "/api/login", options);
+                    console.log('Login response:', response);
+                    if (response.status === 200) {
+                        const data = await response.json();
+                        console.log("access token", data.access_token);
+                        sessionStorage.setItem("token", data.access_token);
+                        sessionStorage.setItem("email", email);
+                        setStore({
+                            token: data.access_token,
+                            email: email,
+                        });
+                        // setIsLoggedIn=true
+                        return true;
+                    } else {
+                        console.error("Login failed. Please check your credentials.");
+                        return false;
+                    }
+                } catch (error) {
+                    console.error("Login error:", error);
+                    alert("An error occurred during login.");
+                    return false;
+                }
+            },
+            signup: async (formData) => {
+                try {
+                    const token = localStorage.getItem("token");
+                    const response = await fetch(process.env.BACKEND_URL + "/api/signup", {
+                        method: "POST",
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({
+                            "email": formData.email,
+                            "password": formData.password,
+                            "first_name": formData.first_name,
+                            "last_name": formData.last_name
+                        })
+                    });
+                    let data = await response.json();
+                    if (data) {
+                        console.log(data.message)
+                        return true;
+                    }
+                } catch (error) {
+                    console.log(error)
+                }
+            },
+            updateAccount: async (userData, pets) => {
+                try {
+                    const token = sessionStorage.getItem("token");
+                    const email = sessionStorage.getItem("email");
+                    console.log({ ...userData, email: email });
+                    const response = await fetch(`${process.env.BACKEND_URL}/api/account`, {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Authorization": `Bearer ${token}`,
+                        },
+                        body: JSON.stringify({
+                            userData: { ...userData, email: email },
+                            pets: pets
+                        }),
+                    });
+                    if (!response.ok) {
+                        throw new Error(`HTTP error! Status: ${response.status}`);
+                    }
+                    return await response.json();
+                } catch (error) {
+                    throw new Error(`Error: ${error.message}`);
+                }
+            },
 			setAccessToken: (savedToken) => {
 				setStore({ token: savedToken })
 			},
@@ -57,38 +106,15 @@ const getState = ({ getStore, getActions, setStore }) => {
 			setTimeslotsStartingDay: (obj) => {
 				setStore({ timeSlotsStartingDay: obj })
 			},
-			signup: async (formData) => {
-				try {
-					let response = await fetch(process.env.BACKEND_URL + "api/signup", {
-						method: "POST",
-						headers: { "Content-Type": "application/json" },
-						body: JSON.stringify({ "email": formData.email, "password": formData.password, "first_name": formData.first_name, "last_name": formData.last_name })
-					})
-					let data = await response.json()
-
-					if (data) {
-						console.log(data.message)
-						return true
-					}
-				} catch (error) { console.log(error) }
-			},
-			signupPet: async (formData) => {
-				try {
-					let response = await fetch(process.env.BACKEND_URL + "api/signupPet", {
-						method: "POST",
-						headers: { "Content-Type": "application/json" },
-						body: JSON.stringify({ "name": formData.name, "bread": formData.bread, "age": formData.age, "description": formData.description, "detailed_care_info": formData.detailed_care_info })
-					})
-				}
-				catch (error) {
-					console.log(error)
-				}
-			},
 			changeActiveScheduleTab: (payload) => {
 				setStore({ activeScheduleTab: payload })
 			},
 			setPaymentSuccessful: (payload) => {
 				setStore({ paymentSuccessful: payload })
+			},
+			logout: async () => {
+                sessionStorage.removeItem("token")
+                setStore({ token: null })
 			}
 		}
 	};
