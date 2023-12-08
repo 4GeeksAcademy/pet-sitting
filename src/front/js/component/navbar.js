a915c364a44047cb8b7d495a5f4fdaab33b3ebd5
Whitney Heacock <134784015+Swtpea1516@users.noreply.github.com>
2023-12-08 23:55:50 +0000

    merge to develpoment

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,12 +1,32 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import dogwalklogo from '../../img/dogwalklogo.png'
 import React, { useState, useContext } from "react";
 import { Context } from "../store/appContext";
 import { Link, useNavigate } from "react-router-dom";
-
 export const Navbar = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false); // Set the initial state to false
   const navigate = useNavigate();
   const { store, actions } = useContext(Context);
-
   const handleLogout = async () => {
     try {
       await actions.logout();
@@ -16,7 +36,6 @@ export const Navbar = () => {
       console.error('An error occurred during logout:', error);
     }
   };
-
   return (
     <>
       <div className="number-Desk">
@@ -26,11 +45,6 @@ export const Navbar = () => {
       </div>
       
         <nav className="navbar-Desk navbar-expand-sm bg-body-tertiary">
-
-
-
-
-
           
             <div className="d-flex justify-content-center text-center ">
               <div className="logoTitle-Desk d-flex justify-content-start" >
@@ -41,13 +55,11 @@ export const Navbar = () => {
           <a className="justify-content-start">
             <img className="logoImg-Desk" src="https://private-user-images.githubusercontent.com/134784015/288147616-1f0d3f25-a656-4faa-a301-092929dc6dc8.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTEiLCJleHAiOjE3MDE3OTc5ODcsIm5iZiI6MTcwMTc5NzY4NywicGF0aCI6Ii8xMzQ3ODQwMTUvMjg4MTQ3NjE2LTFmMGQzZjI1LWE2NTYtNGZhYS1hMzAxLTA5MjkyOWRjNmRjOC5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBSVdOSllBWDRDU1ZFSDUzQSUyRjIwMjMxMjA1JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDIzMTIwNVQxNzM0NDdaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0wMTcxNTVjMzU5MWZmYjBmOTU3MWY2NTM3MTFhNTYxYTc2MmI0NmMzYzNkYzdiMWE1YmU3N2I0NTI2NDFiYzFmJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.yoJ2G1qkTkLDlc8xIND6HgkPYjj1qaQNdk8ITCr47CY" />
           </a>
-
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="navItem-Desk collapse navbar-collapse" id="navbarNav d-md-flex">
             <ul className="navbar-nav  d-flex justify-content-center text-center ">
-
               {!store.token ? (
                 <>
                   <li className="nav-item">
@@ -75,16 +87,15 @@ export const Navbar = () => {
                     <Link to="/payment" className="nav-link " style={{ color: 'black' }}>Payment</Link>
                   </li>
                   <li className="nav-item">
-                    <Link to="/insurance" className="nav-link  "  style={{ color: 'black' }}>Insurance</Link>
+                    <Link to="/insurance" className="nav-link  " aria-disabled="true" style={{ color: 'black' }}>Insurance</Link>
+                 
                   </li>
                   <li className="nav-item">
-                    <Link to="/" className="nav-link " onClick={handleLogout}  style={{ color: 'black' }}>Logout</Link>
+                    <Link to="/" className="nav-link " onClick={handleLogout} aria-disabled="true" style={{ color: 'black' }}>Logout</Link>
                   </li>
                 </>}
 
               <>
-
-
                 {/* <li className="nav-item">
               <Link to="/account" className="nav-link" style={{ color: 'black' }}>Account</Link>
             </li>
@@ -104,12 +115,134 @@ export const Navbar = () => {
             </ul>
             
           </div>
-
       
     </nav >
     </>
   );
 };
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState, useContext } from "react";
+// import { Context } from "../store/appContext";
+// import { Link, useNavigate } from "react-router-dom";
+
+// export const Navbar = () => {
+//   const [isLoggedIn, setIsLoggedIn] = useState(false); // Set the initial state to false
+//   const navigate = useNavigate();
+//   const { store, actions } = useContext(Context);
+
+//   const handleLogout = async () => {
+//     try {
+//       await actions.logout();
+//       setIsLoggedIn(false); // Set isLoggedIn to false on logout
+//       navigate("/");
+//     } catch (error) {
+//       console.error('An error occurred during logout:', error);
+//     }
+//   };
+
+//   return (
+//     <>
+//       <div className="number-Desk">
+//         (123)456-7891
+//         <br></br>
+//         Hotdoggitydogwalker@gmail.com
+//       </div>
+      
+//         <nav className="navbar-Desk navbar-expand-sm bg-body-tertiary">
+
+
+
+
+
+          
+//             <div className="d-flex justify-content-center text-center ">
+//               <div className="logoTitle-Desk d-flex justify-content-start" >
+//                 <h2 className="title-Desk">Hot Doggity Dog Walkers</h2>
+//               </div>
+//             </div>
+          
+//           <a className="justify-content-start">
+//             <img className="logoImg-Desk" src="https://private-user-images.githubusercontent.com/134784015/288147616-1f0d3f25-a656-4faa-a301-092929dc6dc8.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTEiLCJleHAiOjE3MDE3OTc5ODcsIm5iZiI6MTcwMTc5NzY4NywicGF0aCI6Ii8xMzQ3ODQwMTUvMjg4MTQ3NjE2LTFmMGQzZjI1LWE2NTYtNGZhYS1hMzAxLTA5MjkyOWRjNmRjOC5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBSVdOSllBWDRDU1ZFSDUzQSUyRjIwMjMxMjA1JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDIzMTIwNVQxNzM0NDdaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0wMTcxNTVjMzU5MWZmYjBmOTU3MWY2NTM3MTFhNTYxYTc2MmI0NmMzYzNkYzdiMWE1YmU3N2I0NTI2NDFiYzFmJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.yoJ2G1qkTkLDlc8xIND6HgkPYjj1qaQNdk8ITCr47CY" />
+//           </a>
+
+//           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+//             <span className="navbar-toggler-icon"></span>
+//           </button>
+//           <div className="navItem-Desk collapse navbar-collapse" id="navbarNav d-md-flex">
+//             <ul className="navbar-nav  d-flex justify-content-center text-center ">
+
+//               {!store.token ? (
+//                 <>
+//                   <li className="nav-item">
+//                     <Link to="/" className="nav-link " aria-current="page" style={{ color: 'black' }}>Home</Link>
+//                   </li>
+//                   <li className="nav-item">
+//                     <Link to="/signupUser" className="nav-link " style={{ color: 'black' }}>Sign up</Link>
+//                   </li>
+//                 </>
+//               ) :
+//                 <>
+//                   <li className="nav-item">
+//                     <Link to="/" className="nav-link e" aria-current="page" style={{ color: 'black' }}>Home</Link>
+//                   </li>
+//                   <li className="nav-item">
+//                     <Link to="/signupUser" className="nav-link" style={{ color: 'black' }}>Sign up</Link>
+//                   </li>
+//                   <li className="nav-item">
+//                     <Link to="/account" className="nav-link " style={{ color: 'black' }}>Account</Link>
+//                   </li>
+//                   <li className="nav-item">
+//                     <Link to="/services" className="nav-link " style={{ color: 'black' }}>Service</Link>
+//                   </li>
+//                   <li className="nav-item">
+//                     <Link to="/payment" className="nav-link " style={{ color: 'black' }}>Payment</Link>
+//                   </li>
+//                   <li className="nav-item">
+//                     <Link to="/insurance" className="nav-link  "  style={{ color: 'black' }}>Insurance</Link>
+//                   </li>
+//                   <li className="nav-item">
+//                     <Link to="/" className="nav-link " onClick={handleLogout}  style={{ color: 'black' }}>Logout</Link>
+//                   </li>
+//                 </>}
+
+//               <>
+
+
+//                 {/* <li className="nav-item">
+//               <Link to="/account" className="nav-link" style={{ color: 'black' }}>Account</Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link to="/services" className="nav-link" style={{ color: 'black' }}>Service</Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link to="/payment" className="nav-link" style={{ color: 'black' }}>Payment</Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link to="/insurance" className="nav-link " aria-disabled="true" style={{ color: 'black' }}>Insurance</Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link to="/" className="nav-link" onClick={handleLogout} aria-disabled="true" style={{ color: 'black' }}>Logout</Link> */}
+//                 {/* </li> */}
+//               </>
+//             </ul>
+            
+//           </div>
+
+      
+//     </nav >
+//     </>
+//   );
+// };
 
 
 
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,79 +1,62 @@
 const getState = ({ getStore, getActions, setStore }) => {
-
     return {
         store: {
-            backendURL: process.env.BACKEND_URL,
             token: null,
-            message: null,
-            demo: [
-                {
-                    title: "FIRST",
-                    background: "white",
-                    initial: "white"
-                },
-                {
-                    title: "SECOND",
-                    background: "white",
-                    initial: "white"
-                }
-            ],
             timeSlotsStartingDay: {
                 "date": (new Date).getDate(),
                 "month": (new Date).getMonth(),
                 "year": (new Date).getFullYear()
             },
             activeScheduleTab: "nav-timeslots",
-            typeOfSchedule: 'dog-walk'
+            token: null,
+            payPalToken: null,
+            paymentSuccessful: false,
         },
         actions: {
-            // Use getActions to call a function within a fuction
-            setAccessToken: (savedToken) => {
-                setStore({ token: savedToken })
+            changeActiveScheduleTab: (payload) => {
+                setStore({ activeScheduleTab: payload })
             },
             login: async (email, password) => {
                 const store = getStore();
-
                 try {
-                    let options = {
-                        method: "POST",
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify({ email, password }),
-                    };
-
-                    const response = await fetch(process.env.BACKEND_URL + "/api/login", options);
-
-                    console.log('Login response:', response);
-
-                    if (response.status === 200) {
-                        const data = await response.json();
-                        console.log("access token", data.access_token);
-                        sessionStorage.setItem("token", data.access_token);
-                        sessionStorage.setItem("email", email);
-                        setStore({
-                            token: data.access_token,
-                            email: email,
-                        });
-                        // setIsLoggedIn=true
-                        return true;
+                  let options = {
+                    method: "POST",
+                    headers: {
+                      'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ email, password }),
+                  };
+                  const response = await fetch(process.env.BACKEND_URL + "/api/login", options);
+                  console.log('Login response:', response);
+                  if (response.status === 200) {
+                    const data = await response.json();
+                    console.log("access token", data.access_token);
+        
+                    if (data.access_token) {
+                      sessionStorage.setItem("token", data.access_token);
+                      sessionStorage.setItem("email", email);
+                      setStore({
+                        token: data.access_token,
+                        email: email,
+                      });
+                      return true;
                     } else {
-                        console.error("Login failed. Please check your credentials.");
-                        return false;
+                      console.error("Login failed. Please check your credentials.");
+                      return false;
                     }
-                } catch (error) {
-                    console.error("Login error:", error);
-                    alert("An error occurred during login.");
+                  } else {
+                    console.error("Login failed. Please check your credentials.");
                     return false;
+                  }
+                } catch (error) {
+                  console.error("Login error:", error);
+                  alert("An error occurred during login.");
+                  return false;
                 }
-            },
-
-
-           
+              },
             signup: async (formData) => {
                 try {
                     const token = localStorage.getItem("token");
-
                     const response = await fetch(process.env.BACKEND_URL + "/api/signup", {
                         method: "POST",
                         headers: {
@@ -86,9 +69,7 @@ const getState = ({ getStore, getActions, setStore }) => {
                             "last_name": formData.last_name
                         })
                     });
-
                     let data = await response.json();
-
                     if (data) {
                         console.log(data.message)
                         return true;
@@ -97,274 +78,52 @@ const getState = ({ getStore, getActions, setStore }) => {
                     console.log(error)
                 }
             },
-
             updateAccount: async (userData, pets) => {
                 try {
-                    const token = sessionStorage.getItem("token");
-                    const email = sessionStorage.getItem("email");
-                    console.log({ ...userData, email: email });
-                    const response = await fetch(`${process.env.BACKEND_URL}/api/account`, {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json",
-                            "Authorization": `Bearer ${token}`,
-                        },
-                        body: JSON.stringify({
-                            userData: { ...userData, email: email },
-                            pets: pets
-                        }),
-                    });
-
-                    if (!response.ok) {
-                        throw new Error(`HTTP error! Status: ${response.status}`);
-                    }
-
-                    return await response.json();
+                  const token = sessionStorage.getItem("token");
+                  const email = sessionStorage.getItem("email");
+                  console.log({ ...userData, email: email });
+                  const response = await fetch(`${process.env.BACKEND_URL}/api/account`, {
+                    method: "POST",
+                    headers: {
+                      "Content-Type": "application/json",
+                      "Authorization": `Bearer ${token}`,
+                    },
+                    body: JSON.stringify({
+                      userData: { ...userData, email: email },
+                      pets: pets
+                    }),
+                  });
+        
+                  if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                  }
+        
+                  return await response.json();
                 } catch (error) {
-                    throw new Error(`Error: ${error.message}`);
+                  throw new Error(`Error: ${error.message}`);
                 }
+              },
+            setAccessToken: (savedToken) => {
+                setStore({ token: savedToken })
             },
-
-            addPet: async (petData) => {
-                try {
-                    const token = localStorage.getItem("token");
-                    const response = await fetch(`${process.env.BACKEND_URL}/api/pets`, {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json",
-                            "Authorization": `Bearer ${token}`,
-                        },
-                        body: JSON.stringify(petData),
-                    });
-
-                    if (!response.ok) {
-                        throw new Error(`HTTP error! Status: ${response.status}`);
-                    }
-
-                    return await response.json();
-                } catch (error) {
-                    throw new Error(`Error: ${error.message}`);
-                }
+            exampleFunction: () => {
+                getActions().changeColor(0, "green");
+            },
+            setTimeslotsStartingDay: (obj) => {
+                setStore({ timeSlotsStartingDay: obj })
             },
             changeActiveScheduleTab: (payload) => {
                 setStore({ activeScheduleTab: payload })
             },
+            setPaymentSuccessful: (payload) => {
+                setStore({ paymentSuccessful: payload })
+            },
             logout: async () => {
                 sessionStorage.removeItem("token")
                 setStore({ token: null })
-
-
             }
-        },
+        }
     };
-
 }
-
-
-
 export default getState;
-
-
-
-
-
-// 	return {
-// 		store: {
-// 			backendURL: process.env.BACKEND_URL,
-// 			token: null,
-// 			timeSlotsStartingDay: {
-// 				"date": (new Date).getDate(),
-// 				"month": (new Date).getMonth(),
-// 				"year": (new Date).getFullYear()
-// 			},
-// 			activeScheduleTab: "nav-timeslots",
-// 			payPalToken: null,
-// 			paymentSuccessful: false,
-// 			activeScheduleTab: "nav-timeslots",
-// 			typeOfSchedule: 'dog-walk'
-// 		},
-// 		actions: {
-// 			// Use getActions to call a function within a fuction
-// 			login: async (email, password) => {
-// 				const store = getStore();
-// 				try {
-// 					let options = {
-// 						method: "POST",
-// 						headers: {
-// 							'Content-Type': 'application/json',
-// 						},
-// 						body: JSON.stringify({ email, password }),
-// 					};
-// 					const response = await fetch(process.env.BACKEND_URL + "api/login", options);
-// 					console.log('Login response:', response);
-// 					if (response.status === 200) {
-// 						const data = await response.json();
-// 						console.log("access token", data.access_token);
-// 						sessionStorage.setItem("token", data.access_token);
-// 						sessionStorage.setItem("email", email);
-// 						setStore({
-// 							token: data.access_token,
-// 							email: email,
-// 						});
-// 						return true;
-// 					} else {
-// 						console.error("Login failed. Please check your credentials.");
-// 						return false;
-// 					}
-// 				} catch (error) {
-// 					console.error("Login error:", error);
-// 					alert("An error occurred during login.");
-// 					return false;
-// 				}
-// 			},
-// 			setAccessToken: (savedToken) => {
-// 				setStore({ token: savedToken })
-// 			},
-// 			exampleFunction: () => {
-// 				getActions().changeColor(0, "green");
-// 			},
-// 			setTimeslotsStartingDay: (obj) => {
-// 				setStore({ timeSlotsStartingDay: obj })
-// 			},
-// 			signup: async (formData) => {
-// 				try {
-
-// 					let response = await fetch(process.env.BACKEND_URL + "api/signup", {
-// 						method: "POST",
-// 						headers: { "Content-Type": "application/json" },
-// 						body: JSON.stringify({ "email": formData.email, "password": formData.password, "first_name": formData.first_name, "last_name": formData.last_name })
-// 					})
-// 					let data = await response.json()
-
-// 					if (data) {
-// 						console.log(data.message)
-// 						return true
-// 					}
-// 				} catch (error) { console.log(error) }
-// 			},
-// 			updateStoreFromStorage: () => {
-// 				let accessToken = sessionStorage.getItem("token");
-// 				let userString = sessionStorage.getItem("user");
-// 				let userObject = JSON.parse(userString);
-// 				if (accessToken && accessToken != "" && accessToken != "undefined") {
-// 					setStore({ accessToken: accessToken });
-// 					setStore({ user: userObject });
-// 				}
-// 			},
-
-// 			logUserInTheStore: (data) => {
-// 				setStore({
-// 					user: data.user,
-// 					accessToken: data.token,
-// 					activeuser: data.user.id,
-// 				});
-// 				sessionStorage.setItem("token", data.token);
-// 				sessionStorage.setItem("user", JSON.stringify(data.user));
-// 			},
-
-// 			//   resetPassword: (token, newPassword) => {
-// 			// 	const store = getStore();
-// 			// 	return fetch("/api/reset-password", {
-// 			// 		method: 'PUT',
-// 			// 		headers: { "Content-Type": "application/json" },
-// 			// 		body: JSON.stringify({ token: token, new_password: newPassword }),
-// 			// 	})
-// 			// 	.then(response => {
-// 			// 		if (response.ok) {
-// 			// 			return response.json();
-// 			// 		} else {
-// 			// 			throw new Error('Error resetting password.');
-// 			// 		}
-// 			// 	})
-// 			// 	.catch(error => {
-// 			// 		console.error(error);
-// 			// 		throw error;
-// 			// 	});
-// 			// }
-
-// 			resetPassword: (token, newPassword) => {
-// 				const store = getStore();
-// 				console.log("Reset Password Request:", process.env.BACKEND_URL + "/api/reset-password");
-// 				console.log("Token:", token);
-// 				console.log("New Password:", newPassword);
-
-// 				return fetch(process.env.BACKEND_URL + "/api/reset-password", {
-// 					method: 'POST',
-// 					headers: { "Content-Type": "application/json" },
-// 					body: JSON.stringify({ token: token, new_password: newPassword }),
-// 				})
-// 					.then(response => {
-// 						console.log("Reset Password Response:", response);
-
-// 						if (response.ok) {
-// 							return response.json();
-// 						} else {
-// 							throw new Error('Error resetting password.');
-// 						}
-// 					})
-// 					.catch(error => {
-// 						console.error("Reset Password Error:", error);
-// 						throw error;
-// 					});
-// 			}
-
-// 		},
-
-// 	};
-// }
-
-
-
-
-// 			signupPet: async (formData) => {
-// 				try {
-// 					let response = await fetch(process.env.BACKEND_URL + "api/signupPet", {
-// 						method: "POST",
-// 						headers: { "Content-Type": "application/json" },
-// 						body: JSON.stringify({ "name": formData.name, "bread": formData.bread, "age": formData.age, "description": formData.description, "detailed_care_info": formData.detailed_care_info })
-// 					})
-// 				}
-// 				catch (error) {
-// 					console.log(error)
-// 				}
-// 			},
-// 			changeActiveScheduleTab: (payload) => {
-// 				setStore({ activeScheduleTab: payload })
-// 			},
-// 			setPaymentSuccessful: (payload) => {
-// 				setStore({ paymentSuccessful: payload })
-// 			}
-// 		}
-// 	};
-// }
-
-
-// export default getState;
-
-
-
-
-
-
-
-
-// 			signupPet: async (formData) => {
-// 				try {
-// 					let response = await fetch(process.env.BACKEND_URL + "api/signupPet", {
-// 						method: "POST",
-// 						headers: { "Content-Type": "application/json" },
-// 						body: JSON.stringify({ "name": formData.name, "bread": formData.bread, "age": formData.age, "description": formData.description, "detailed_care_info": formData.detailed_care_info })
-// 					})
-// 				}
-// 				catch (error) {
-// 					console.log(error)
-// 				}
-// 			},
-// 			changeActiveScheduleTab: (payload) => {
-// 				setStore({ activeScheduleTab: payload })
-// 			}
-// 		}
-// 	}
-// }
-
-
