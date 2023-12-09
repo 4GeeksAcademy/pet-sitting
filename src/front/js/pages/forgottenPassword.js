8c3d02e86aa9f9b66bf562daff3f1fe7446c253f
John Durtka <139003527+JohnD42@users.noreply.github.com>
2023-12-09 01:33:08 +0000

    bugfixed reset password stuff

--- src/front/js/pages/forgottenPassword.js
+++ src/front/js/pages/forgottenPassword.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from 'react-router-dom'
 
 export const ForgottenPassword = () => {
 
@@ -26,7 +27,7 @@ export const ForgottenPassword = () => {
         }
         catch (error) {
             console.log(error)
-            alert (error)
+            alert(error)
         }
     }
     switch (messageStatus) {
@@ -45,14 +46,14 @@ export const ForgottenPassword = () => {
         case "sent":
             return (
                 <div className="container pt-3">
-                <div className="Card mx-auto text-center " style={{ width: "18rem" }}>
-                    <div className="card-body">
-                        <h5 className="card-title">Success!</h5>
-                        <p className="card-text">Check your email!</p>
-                        <p className="card-text"> You'll receive an email shortly with a reset password link</p>
-                        <Link to="/"><a href="#" className="btn btn-primary">Go to the home page</a></Link>
+                    <div className="Card mx-auto text-center " style={{ width: "18rem" }}>
+                        <div className="card-body">
+                            <h5 className="card-title">Success!</h5>
+                            <p className="card-text">Check your email!</p>
+                            <p className="card-text"> You'll receive an email shortly with a reset password link</p>
+                            <Link to="/"><a href="#" className="btn btn-primary">Go to the home page</a></Link>
+                        </div>
                     </div>
-                </div>
                 </div>
             )
             break;
@@ -62,10 +63,10 @@ export const ForgottenPassword = () => {
 
     return (
         <div className="container pt-3"  >
-            <div className="Card mx-auto" style={{width:"25rem"}}>
+            <div className="Card mx-auto" style={{ width: "25rem" }}>
                 <h3 class="text-center">Reset your password</h3>
                 <p class="text-center">Enter the email address registered with your account. We'll email you a temporary password, so that you may access your account and change your password.</p>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email"/>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email" />
                 <button className="btn btn-outline-primary mt-3 w-100">Continue</button>
             </div>
 
