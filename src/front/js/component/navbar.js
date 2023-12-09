b7a4b9dde8e3d5f666288b3e76224efcd181a6d2
John Durtka <139003527+JohnD42@users.noreply.github.com>
2023-12-09 00:34:02 +0000

    updated styling

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -24,62 +24,62 @@ export const Navbar = () => {
         <br></br>
         Hotdoggitydogwalker@gmail.com
       </div>
-      
-        <nav className="navbar-Desk navbar-expand-sm bg-body-tertiary">
-          
-            <div className="d-flex justify-content-center text-center ">
-              <div className="logoTitle-Desk d-flex justify-content-start" >
-                <h2 className="title-Desk">Hot Doggity Dog Walkers</h2>
-              </div>
-            </div>
-          
-            
+
+      <nav className="navbar-Desk navbar-expand-sm bg-body-tertiary">
+
+        <div className="d-flex justify-content-center text-center ">
+          <div className="logoTitle-Desk d-flex justify-content-start" >
+            <h2 className="title-Desk">Hot Doggity Dog Walkers</h2>
+          </div>
+        </div>
+
+
         <a className="justify-content-start">
           <img className="logoImg-Desk custom-logo" src={dogwalklogo} />
         </a>
-         
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="navItem-Desk collapse navbar-collapse" id="navbarNav d-md-flex">
-            <ul className="navbar-nav  d-flex justify-content-center text-center ">
-              {!store.token ? (
-                <>
-                  <li className="nav-item">
-                    <Link to="/" className="nav-link " aria-current="page" style={{ color: 'black' }}>Home</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link to="/signupUser" className="nav-link " style={{ color: 'black' }}>Sign up</Link>
-                  </li>
-                </>
-              ) :
-                <>
-                  <li className="nav-item">
-                    <Link to="/" className="nav-link e" aria-current="page" style={{ color: 'black' }}>Home</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link to="/signupUser" className="nav-link" style={{ color: 'black' }}>Sign up</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link to="/account" className="nav-link " style={{ color: 'black' }}>Account</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link to="/services" className="nav-link " style={{ color: 'black' }}>Service</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link to="/payment" className="nav-link " style={{ color: 'black' }}>Payment</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link to="/insurance" className="nav-link  " aria-disabled="true" style={{ color: 'black' }}>Insurance</Link>
-                 
-                  </li>
-                  <li className="nav-item">
-                    <Link to="/" className="nav-link " onClick={handleLogout} aria-disabled="true" style={{ color: 'black' }}>Logout</Link>
-                  </li>
-                </>}
 
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="navItem-Desk collapse navbar-collapse" id="navbarNav d-md-flex">
+          <ul className="navbar-nav  d-flex justify-content-center text-center ">
+            {!store.token ? (
               <>
-                {/* <li className="nav-item">
+                <li className="nav-item">
+                  <Link to="/" className="nav-link " aria-current="page" style={{ color: 'black' }}>Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/signupUser" className="nav-link " style={{ color: 'black' }}>Sign up</Link>
+                </li>
+              </>
+            ) :
+              <>
+                <li className="nav-item">
+                  <Link to="/" className="nav-link e" aria-current="page" style={{ color: 'black' }}>Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/signupUser" className="nav-link" style={{ color: 'black' }}>Sign up</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/account" className="nav-link " style={{ color: 'black' }}>Account</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/services" className="nav-link " style={{ color: 'black' }}>Services</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/payment" className="nav-link " style={{ color: 'black' }}>Payment</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/insurance" className="nav-link  " aria-disabled="true" style={{ color: 'black' }}>Insurance</Link>
+
+                </li>
+                <li className="nav-item">
+                  <Link to="/" className="nav-link " onClick={handleLogout} aria-disabled="true" style={{ color: 'black' }}>Logout</Link>
+                </li>
+              </>}
+
+            <>
+              {/* <li className="nav-item">
               <Link to="/account" className="nav-link" style={{ color: 'black' }}>Account</Link>
             </li>
             <li className="nav-item">
@@ -93,13 +93,13 @@ export const Navbar = () => {
             </li>
             <li className="nav-item">
               <Link to="/" className="nav-link" onClick={handleLogout} aria-disabled="true" style={{ color: 'black' }}>Logout</Link> */}
-                {/* </li> */}
-              </>
-            </ul>
-            
-          </div>
-      
-    </nav >
+              {/* </li> */}
+            </>
+          </ul>
+
+        </div>
+
+      </nav >
     </>
   );
 };
@@ -108,7 +108,7 @@ export const Navbar = () => {
 
 
 
-      
+
 
 
 
