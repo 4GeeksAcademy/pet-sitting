1fc3553ca2eff82597e1525f5b07cb200e91fed1
IvanStacy <139801928+IvanStacy@users.noreply.github.com>
2023-12-12 05:40:16 +0000

    working on mobile version need to finish the footer with pictures

--- src/front/js/component/MobileHome.js
+++ src/front/js/component/MobileHome.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState, useEffect } from "react";
 import { Context } from "../store/appContext";
 import { Link, useNavigate } from "react-router-dom";
+// import {MobileHome.css} from "../../styles/MobileHome.css"
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
@@ -55,7 +56,7 @@ export const Home = () => {
   return (
     <div className="container-fluid homePage">
       <div className="row homePageText">
-        <h4 className="col-auto">
+        <h6 className="col-auto">
           Having a dedicated pet caregiver provides priceless peace of mind,
           ensuring your furry friend gets the attention and care they need. As a
           committed caregiver, I address both basic needs and understand their
@@ -63,7 +64,7 @@ export const Home = () => {
           health and happiness, fostering both interaction and exercise. This
           bond positively impacts their mental and physical well-being, ensuring
           a fulfilling life in your absence.
-        </h4>
+        </h6>
       </div>
 
 
@@ -71,130 +72,132 @@ export const Home = () => {
 
       <div className="row homePageMiddleRow  d-md-none ">
         <div className="container d-flex">
-          <div className="aboutMePic">
-            <img
-              src="https://cdn.pixabay.com/photo/2015/03/14/05/37/beagle-672798_1280.jpg"
-              className="img-fluid rounded"
-              alt="Dog Pic"
-            />
-            <Link to="/AboutMe" className="btn btn-secondary aboutMeButton">About Me Please Click Here</Link>
-          </div>
+          <div className="row row-cols-1 row-cols-md-4">
+            <div className="aboutMePic">
+              <img
+                src="https://cdn.pixabay.com/photo/2015/03/14/05/37/beagle-672798_1280.jpg"
+                className="img-fluid rounded"
+                alt="Dog Pic"
+              />
+              <Link to="/AboutMe" className="btn btn-secondary aboutMeButton">About Me</Link>
+            </div>
 
-          <div className="center processBox">
-          {greeting}
-            {!sessionStorage.getItem("token") && (
-              <button
-                type="button"
-                className="btn btn-secondary loginButton"
-                data-bs-toggle="modal"
-                data-bs-target="#exampleModal"
-                onClick={() => setShowModal(true)}
+            <div className="center processBox">
+              {greeting}
+              {!sessionStorage.getItem("token") && (
+                <button
+                  type="button"
+                  className="btn btn-secondary loginButton"
+                  data-bs-toggle="modal"
+                  data-bs-target="#exampleModal"
+                  onClick={() => setShowModal(true)}
+                >
+                  Log in
+                </button>
+              )}
+
+
+              <div
+                className="modal fade"
+                id="exampleModal"
+                tabIndex="-1"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+                style={{ display: showModal ? 'block' : 'none' }}
+                onClick={() => setShowModal(false)}
               >
-                Log in
-              </button>
-            )}
-
-
-            <div
-              className="modal fade"
-              id="exampleModal"
-              tabIndex="-1"
-              aria-labelledby="exampleModalLabel"
-              aria-hidden="true"
-              style={{ display: showModal ? 'block' : 'none' }}
-              onClick={() => setShowModal(false)}
-            >
-              <div className="modal-dialog">
-                <div className="modal-content">
-                  <div className="modal-header">
-                    {/* Close button (x) to close the modal */}
-                    <button
-                      type="button"
-                      className="btn-close"
-                      data-bs-dismiss="modal"
-                      aria-label="Close"
-                      onClick={() => setShowModal(false)}
-                    ></button>
-                  </div>
-                  <div className="modal-body">
-                    <div>
-                      <div className="mb-3">
-                        <label
-                          htmlFor="exampleInputEmail1"
-                          className="form-label"
-                        >
-                          Email address
-                        </label>
-                        <input
-                          type="email"
-                          className="form-control"
-                          id="exampleInputEmail1"
-                          aria-describedby="emailHelp"
-                          value={email}
-                          onChange={(e) => setEmail(e.target.value)}
-                        />
-                        <div id="emailHelp" className="form-text"></div>
-                      </div>
-                      <div className="mb-3">
-                        <label
-                          htmlFor="exampleInputPassword1"
-                          className="form-label"
-                        >
-                          Password
-                        </label>
-                        <input
-                          type="password"
-                          className="form-control"
-                          id="exampleInputPassword1"
-                          value={password}
-                          onChange={(e) => setPassword(e.target.value)}
-                        />
-                      </div>
-
-                      <div
-                        className={`modal fade ${showModal ? 'show' : ''}`}
-                        id="exampleModal"
-                        tabIndex="-1"
-                        aria-labelledby="exampleModalLabel"
-                        aria-hidden="true"
-                        style={{ display: showModal ? 'block' : 'none' }}
+                <div className="modal-dialog">
+                  <div className="modal-content">
+                    <div className="modal-header">
+                      {/* Close button (x) to close the modal */}
+                      <button
+                        type="button"
+                        className="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
                         onClick={() => setShowModal(false)}
-                      ></div>
+                      ></button>
                     </div>
-                  </div>
+                    <div className="modal-body">
+                      <div>
+                        <div className="mb-3">
+                          <label
+                            htmlFor="exampleInputEmail1"
+                            className="form-label"
+                          >
+                            Email address
+                          </label>
+                          <input
+                            type="email"
+                            className="form-control"
+                            id="exampleInputEmail1"
+                            aria-describedby="emailHelp"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                          />
+                          <div id="emailHelp" className="form-text"></div>
+                        </div>
+                        <div className="mb-3">
+                          <label
+                            htmlFor="exampleInputPassword1"
+                            className="form-label"
+                          >
+                            Password
+                          </label>
+                          <input
+                            type="password"
+                            className="form-control"
+                            id="exampleInputPassword1"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                          />
+                        </div>
+
+                        <div
+                          className={`modal fade ${showModal ? 'show' : ''}`}
+                          id="exampleModal"
+                          tabIndex="-1"
+                          aria-labelledby="exampleModalLabel"
+                          aria-hidden="true"
+                          style={{ display: showModal ? 'block' : 'none' }}
+                          onClick={() => setShowModal(false)}
+                        ></div>
+                      </div>
+                    </div>
 
 
-                  <div className="modal-footer d-flex justify-content-center align-items-center">
-                    <button
-                      type="button"
-                      className="btn btn-secondary loginButton"
-                      data-bs-toggle="modal"
-                      data-bs-target="#exampleModal"
-                      onClick={handleLogin} >
-                      submit
-                    </button>
+                    <div className="modal-footer d-flex justify-content-center align-items-center">
+                      <button
+                        type="button"
+                        className="btn btn-secondary loginButton"
+                        data-bs-toggle="modal"
+                        data-bs-target="#exampleModal"
+                        onClick={handleLogin} >
+                        submit
+                      </button>
 
-                    <Link to="/forgotten-password" className="forgotPasswordLink ms-2">
-                      Forgot Password?
-                    </Link>
+                      <Link to="/forgotten-password" className="forgotPasswordLink ms-2">
+                        Forgot Password?
+                      </Link>
 
 
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
 
 
-          <div className="starNowPic">
-            <img
-              src="https://cdn.pixabay.com/photo/2023/10/26/15/33/dog-training-8342962_1280.jpg"
-              className="img-fluid rounded"
-              alt="Dog Pic"
-            />
+            <div className="starNowPic">
+              <img
+                src="https://cdn.pixabay.com/photo/2023/10/26/15/33/dog-training-8342962_1280.jpg"
+                className="img-fluid rounded"
+                alt="Dog Pic"
+              />
 
-            <Link to="/signupUser" className=" btn btn-secondary StartNowText">Click here to join the family</Link>
+              <Link to="/signupUser" className=" btn btn-secondary StartNowText">Click here to join the family</Link>
 
+            </div>
           </div>
         </div>
       </div>
--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -17,46 +17,53 @@ export const Footer = () => {
     <footer className="footer mt-auto py-3 text-center">
       <div className=" footerScrollBar container">
         <div className=" allCardItems row">
+          
 
-          <div className="scrollable-content " style={{ overflowX: 'auto', Width: '100%' }}>
-            <img src="https://cdn.pixabay.com/photo/2019/12/30/05/30/cockatiel-4728876_1280.jpg" className="d-block w-100" alt="" />
-            <p>
+          <div className="scard">
+            <img src="https://cdn.pixabay.com/photo/2019/12/30/05/30/cockatiel-4728876_1280.jpg" className="card-img-top" alt="" />
+            <div className="card-body">
+            <p className="card-text text-center">
               Kelly deserves a glowing 5-star review!
-              Their exceptional care for my birds during my beach trip exceeded my expectations.
-              Regular updates and a relaxing return home make her the top choice for pet sitting.
+              Their exceptional care <br></br> for my birds during my beach trip exceeded my expectations.<br></br>
+              Regular updates and a relaxing return home make he <br></br> the top choice for pet sitting.<br></br>
               Highly recommend this reliable and caring service! ⭐️⭐️⭐️⭐️⭐️
             </p>
+            </div>
           </div>
 
 
-          <div className="scrollable-content" style={{ overflowX: 'auto', Width: '25%' }}>
-            <img src="https://cdn.pixabay.com/photo/2018/04/23/14/38/dog-3344414_1280.jpg" className="d-block w-100" alt="..." />
-            <p>
-              Kelly, the dog walker, is a miracle worker!
-              She did an incredible job with my usually shy dog,
-              making every walk a joyful experience.
-              Her patience and expertise deserve a solid 5 stars.
+          <div className="scard" >
+            <img src="https://cdn.pixabay.com/photo/2018/04/23/14/38/dog-3344414_1280.jpg" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <p className="card-text">
+              Kelly, the dog walker, is a miracle worker!<br></br>
+              She did an incredible job with my usually shy dog,<br></br>
+              making every walk a joyful experience.<br></br>
+              Her patience and expertise deserve a solid 5 stars.<br></br>
               ⭐️⭐️⭐️⭐️⭐️
             </p>
+            </div>
           </div>
 
 
-          <div className="scrollable-content" style={{ overflowX: 'auto', Width: '25%' }}>
-            <img src="https://cdn.pixabay.com/photo/2017/08/02/10/01/people-2570587_1280.jpg" className="d-block w-100" alt="..." />
+          <div className="scard">
+            <img src="https://cdn.pixabay.com/photo/2017/08/02/10/01/people-2570587_1280.jpg" className="card-img-top" alt="..." />
+            <div className="card-body">
             <p>
-              Kelly is a lifesaver! While I was on a business trip,
-              she took exceptional care of my cat, ensuring timely and nourishing meals.
-              Her attention to detail and reliability earn her a well-deserved 5-star rating. ⭐️⭐️⭐️⭐️⭐️
+              Kelly is a lifesaver! While I was on a business trip,<br></br>
+              she took exceptional care of my cat,<br></br> ensuring timely and nourishing meals..<br></br>
+              Her attention to detail and reliability earn her a well-deserved <br></br>5-star rating. ⭐️⭐️⭐️⭐️⭐️
             </p>
-
+          </div>
           </div>
 
-          <div className="scrollable-content" style={{ overflowX: 'auto', Width: '25%' }}>
-            <img src="https://cdn.pixabay.com/photo/2022/01/27/23/35/rabbit-6973396_1280.jpg" className="d-block w-100" alt="..." />
+          <div className="scard">
+            <img src="https://cdn.pixabay.com/photo/2022/01/27/23/35/rabbit-6973396_1280.jpg" className="card-img-top" alt="..." />
+            <div className="card-body">
             <p>
-             Kelly did an amazing job watching my pet rabbit Rocky.I will be using her services again! ⭐️⭐️⭐️⭐️⭐️
+             Kelly did an amazing job watching my pet rabbit Rocky..<br></br>I will be using her services again! ⭐️⭐️⭐️⭐️⭐️
             </p>
-
+          </div>
           </div>
         </div>
       </div>
